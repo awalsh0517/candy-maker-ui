2b01b3ab20332f5b920636d42329742be2f3f266
Andrea Walsh <andreawalsh@Andreas-Air.fios-router.home>
2020-05-27 21:47:14 -0400

    Not sure why it is giving me an error with div's

---
+++ components/candies.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default () => (
+  <div className="page">
+    <div className="title">Candy Maker</div>
+    <div className="subtitle">A searchable list of all your favorite candies</div>
+    <input type="text" name="search" />
+    <div className="candy"></div>
+  </div>
+)
